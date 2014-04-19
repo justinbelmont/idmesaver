1bd7027136c17f05c925a266922c4f47e592ac9b
justinbelmont <justinbelmont@gmail.com>
2014-04-19 17:54:41 -0400

    updated javascripts

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$(function(){ 
+$(".mens").hide();
+$(".womens").hide();
+});
+
+ $(".mennav").children("button").click(function(){
+   $(".mens").toggle(1000);
+ });
+
+
+ $(".womennav").children("button").click(function(){
+   $(".womens").toggle(1000);
+ });
+});

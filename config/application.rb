c73cc649c4d5d3a0a934e706910943e4a6f00989
justinbelmont <justinbelmont@gmail.com>
2014-04-21 11:08:16 -0400

    testing

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module Studentshop
   class Application < Rails::Application
     config.assets.precompile += %w( jquery )
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

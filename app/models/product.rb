87122e7a97ebbf0dafe7363858d702f168316279
justinbelmont <justinbelmont@gmail.com>
2014-04-21 10:32:08 -0400

    more retailers

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,14 +6,25 @@ class Product < ActiveRecord::Base
 require 'rest_client'
 
 CONVERSE_DISCOUNT = 0.20
+THEORY_DISCOUNT = 0.20
+JCREW_DISCOUNT = 0.15
+FAHERTY_DISCOUNT = 0.20
 
 def initialize
 
 # GET ALL THE DATA 
   conversemen = RestClient.get 'http://www.kimonolabs.com/api/6wl3jrra?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
   conversewomen = RestClient.get 'http://www.kimonolabs.com/api/d5zydwiu?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+  
   theorywomen = RestClient.get 'http://www.kimonolabs.com/api/c7ui87gw?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
   theorymen = RestClient.get 'http://www.kimonolabs.com/api/4kwjdn8g?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+
+  jcrewwomen = RestClient.get 'http://www.kimonolabs.com/api/4qjzgxmk?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+  jcrewmen = RestClient.get 'http://www.kimonolabs.com/api/3hznva6u?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+
+  fahertywomen = RestClient.get 'http://www.kimonolabs.com/api/4nj9de08?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+  fahertymen = RestClient.get 'http://www.kimonolabs.com/api/8be070ku?apikey=85bcefb6f51d73a5223de8528c4fb2dc'
+
 #PARSE AND FORMAT ALL THE DATA
 #CONVERSE
   @parsed_conversemen = JSON.parse(conversemen)
@@ -28,6 +39,21 @@ def initialize
 
   @parsed_theory_m = JSON.parse(theorymen)
   @theory_m = @parsed_theory_m["results"]["collection1"]  
+
+#JCREW
+  @parsed_jcrew_w = JSON.parse(jcrewwomen)
+  @jcrew_w = @parsed_jcrew_w["results"]
+
+  @parsed_jcrew_m = JSON.parse(jcrewmen)
+  @jcrew_m = @parsed_jcrew_m["results"] 
+
+#FAHERTY
+
+  @parsed_faherty_w = JSON.parse(fahertywomen)
+  @faherty_w = @parsed_faherty_w["results"]["collection1"]
+
+  @parsed_faherty_m = JSON.parse(fahertymen)
+  @faherty_m = @parsed_faherty_m["results"]["collection1"]
 
 end
 
@@ -102,13 +128,13 @@ end
 
 def theory_m_student_price(item)
   retail_price = @theory_m[item]["theory-men-price"].gsub(/[^\d\.]/, '').to_i
-  student_price = retail_price * (1 - CONVERSE_DISCOUNT)
+  student_price = retail_price * (1 - THEORY_DISCOUNT)
   number_to_currency(student_price.to_i)
 end
 
 def theory_m_saved(item)
   retail_price = @theory_m[item]["theory-men-price"].gsub(/[^\d\.]/, '').to_i
-  student_price = retail_price * (1 - CONVERSE_DISCOUNT)
+  student_price = retail_price * (1 - THEORY_DISCOUNT)
   saved = retail_price - student_price
   number_to_currency(saved.to_i)
 end
@@ -131,13 +157,121 @@ end
 
 def theory_w_student_price(item)
   retail_price = @theory_w[item]["theory-women-prices"].gsub(/[^\d\.]/, '').to_i
-  student_price = retail_price * (1 - CONVERSE_DISCOUNT)
+  student_price = retail_price * (1 - THEORY_DISCOUNT)
   number_to_currency(student_price.to_i)
 end
 
 def theory_w_saved(item)
   retail_price = @theory_w[item]["theory-women-prices"].gsub(/[^\d\.]/, '').to_i
-  student_price = retail_price * (1 - CONVERSE_DISCOUNT)
+  student_price = retail_price * (1 - THEORY_DISCOUNT)
+  saved = retail_price - student_price
+  number_to_currency(saved.to_i)
+end
+
+# MENS JCREW
+
+def jcrew_m_retail_price(item)
+  @jcrew_m["collection2"][item]["jcrew-mens-prices"]["text"]
+end
+
+def jcrew_m_product_image(item)
+  @jcrew_m["collection1"][item]["jcrew-mens-imgs"]["src"]
+end
+
+def jcrew_m_product_title(item)
+  @jcrew_m["collection2"][item]["jcrew-mens-titles"]["text"].truncate(30)
+end
+
+def jcrew_m_student_price(item)
+  retail_price = @jcrew_m["collection2"][item]["jcrew-mens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - JCREW_DISCOUNT)
+  number_to_currency(student_price.to_i)
+end
+
+def jcrew_m_saved(item)
+  retail_price = @jcrew_m["collection2"][item]["jcrew-mens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - JCREW_DISCOUNT)
+  saved = retail_price - student_price
+  number_to_currency(saved.to_i)
+end
+
+# WOMENS JCREW
+
+def jcrew_w_retail_price(item)
+  @jcrew_w["collection2"][item]["jcrew-womens-prices"]["text"]
+end
+
+def jcrew_w_product_image(item)
+  @jcrew_w["collection1"][item]["jcrew-womens-imgs"]["src"]
+end
+
+def jcrew_w_product_title(item)
+  @jcrew_w["collection2"][item]["jcrew-womens-titles"]["text"].truncate(30)
+end
+
+def jcrew_w_student_price(item)
+  retail_price = @jcrew_w["collection2"][item]["jcrew-womens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - JCREW_DISCOUNT)
+  number_to_currency(student_price.to_i)
+end
+
+def jcrew_w_saved(item)
+  retail_price = @jcrew_w["collection2"][item]["jcrew-womens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - JCREW_DISCOUNT)
+  saved = retail_price - student_price
+  number_to_currency(saved.to_i)
+end
+
+# WOMEN'S FAHERTY
+
+def faherty_w_retail_price(item)
+  @faherty_w[item]["faherty-womens-prices"]["text"]
+end
+
+def faherty_w_product_image(item)
+  @faherty_w[item]["faherty-womens-imgs"]["src"]
+end
+
+def faherty_w_product_title(item)
+  @faherty_w[item]["faherty-womens-titles"]["text"].truncate(35)
+end
+
+def faherty_w_student_price(item)
+  retail_price = @faherty_w[item]["faherty-womens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - FAHERTY_DISCOUNT)
+  number_to_currency(student_price.to_i)
+end
+
+def faherty_w_saved(item)
+  retail_price = @faherty_w[item]["faherty-womens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - FAHERTY_DISCOUNT)
+  saved = retail_price - student_price
+  number_to_currency(saved.to_i)
+end
+
+# MEN'S FAHERTY
+
+def faherty_m_retail_price(item)
+  @faherty_m[item]["faherty-mens-prices"]["text"]
+end
+
+def faherty_m_product_image(item)
+  @faherty_m[item]["faherty-mens-imgs"]["src"]
+end
+
+def faherty_m_product_title(item)
+  @faherty_m[item]["faherty-mens-titles"]["text"].truncate(35)
+end
+
+def faherty_m_student_price(item)
+  retail_price = @faherty_m[item]["faherty-mens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - FAHERTY_DISCOUNT)
+  number_to_currency(student_price.to_i)
+end
+
+def faherty_m_saved(item)
+  retail_price = @faherty_m[item]["faherty-mens-prices"]["text"].gsub(/[^\d\.]/, '').to_i
+  student_price = retail_price * (1 - FAHERTY_DISCOUNT)
   saved = retail_price - student_price
   number_to_currency(saved.to_i)
 end
